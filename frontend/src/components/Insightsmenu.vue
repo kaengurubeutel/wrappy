638c1b5174c2f8cbc6f6dbe314344f991eb9ba08
Maximilian Uhlig <maximilian.uhlig@hs-augsburg.de>
2022-07-23 17:30:32 +0200

    bugfixing

---
+++ frontend/src/components/Insightsmenu.vue
@@ -0,0 +1,29 @@
+
+<script>
+
+import { states } from '../states/state'
+
+
+export default {
+  data() {
+    return {
+      states
+    }
+  },
+  methods:{
+  
+  }
+}
+</script>
+
+
+<template>
+
+  
+  
+</template>
+
+
+<style scoped>
+
+</style>
